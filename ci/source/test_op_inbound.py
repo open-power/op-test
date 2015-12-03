eae523f104ae8a24dbd971daccc9b0827a8cb1c6
Pavaman Subramaniyam <pavsubra@linux.vnet.ibm.com>
2015-12-03 05:48:56 -0500

    Added addidtional command line parameters for getting lpar IP address, lpar user, lpar password, hpmimage respectively in op-ci-bvt Added addidtional command line parameters for getting lpar IP address, lpar user, lpar password, hpmimage respectively in op-ci-setup.xml Added the new arguments lparip, lparuser, lparPasswd and hpmimage to be used in setting up the config file in run-bvt-setup Developed new ci source script files op_inbound_hpm.py and test_op_inbound.py to verify the functionalities of OpTestLpar.py The new file op_inbound_hpm.py contains the test functions for enabling LPAR communication in OpenPower systems Also developed new op-inbound-basic.xml and op-inbound-basic-bvt.xml test xml files The test case file op-inbound-basic.xml tests the get_OS_Level, cold_reset, protect_network_setting and HPM code_update functioanlities of lpar

---
+++ ci/source/test_op_inbound.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: op-auto-test/ci/source/test_op_inbound.py $
+#
+# OpenPOWER Automated Test Project
+#
+# Contributors Listed Below - COPYRIGHT 2015
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+import os
+import sys
+#from op_ci_bmc import bmc_reboot
+#full_path = os.path.abspath(os.path.dirname(sys.argv[0])).split('ci')[0]
+#sys.path.append(full_path)
+#import op_ci_bmc
+# Fixture
+import op_ci_bmc
+import op_inbound_hpm
+
+def test_get_OS_Level():
+    assert op_inbound_hpm.get_OS_Level() == 0
+
+def test_cold_reset():
+    assert op_inbound_hpm.cold_reset() == 0
+
+def test_protect_network_setting():
+    assert op_inbound_hpm.protect_network_setting() == 0
+
+def test_code_update():
+    assert op_inbound_hpm.code_update() == 0
+
