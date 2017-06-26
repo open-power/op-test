77e32b2a41b9d85418397b04cda50e6d9a5ddf96
Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>
2017-06-26 13:33:41 +0530

    PCI Test enhancements.
    Where one test compares pci device discovery with soft and hard reboots
    of the system.
    hard boot--> Full power off followed by power on.
    soft reboot--> reboot command in hostos at runtime
    Example BZ: 155021
    Where nvme devices failed to discover during OS reboot, whereas on
    Hard boot it is able to discover.
    
    So this testcase can un-cover these types of bugs.
    
    Another test is to compare pci device discovery with skiroot
    and distro OS output.
    
    Signed-off-by: Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>

--- testcases/OpTestPCI.py
+++ testcases/OpTestPCI.py
@@ -35,11 +35,13 @@ import commands
 import re
 import sys
 import os
+import pexpect
 import os.path
 
 import unittest
 
 import OpTestConfiguration
+from common.OpTestConstants import OpTestConstants as BMC_CONST
 from common.OpTestSystem import OpSystemState
 from common.Exceptions import CommandFailed
 
@@ -76,7 +78,8 @@ class TestPCI():
         # from "ssh user@host lspci -mm -n > host-lspci.txt" so that the diff
         # is simple to do
         self.pci_data_hostos = '\n'.join(l_res) + '\n'
-        diff_process = subprocess.Popen(['diff', "-u", self.pci_good_data_file , "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        diff_process = subprocess.Popen(['diff', "-u", self.pci_good_data_file , "-"],
+                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         diff_stdout, diff_stderr = diff_process.communicate(self.pci_data_hostos)
         r = diff_process.wait()
         self.assertEqual(r, 0, "Stored and detected PCI devices differ:\n%s%s" % (diff_stdout, diff_stderr))
@@ -135,4 +138,79 @@ class PcieLinkErrorsSkiroot(TestPCISkiroot, unittest.TestCase):
     def runTest(self):
         self.setup_test()
         self.pcie_link_errors()
+
+class TestPciSkirootReboot(TestPCI, unittest.TestCase):
+
+    def set_up(self):
+        self.test = "skiroot_reboot"
+
+    def runTest(self):
+        self.set_up()
+        c = self.cv_SYSTEM.sys_get_ipmi_console()
+        self.cv_SYSTEM.goto_state(OpSystemState.OFF)
+        if "skiroot_reboot" in self.test:
+            self.cv_SYSTEM.goto_state(OpSystemState.PETITBOOT_SHELL)
+        elif "distro_reboot" in self.test:
+            self.cv_SYSTEM.goto_state(OpSystemState.OS)
+            self.cv_SYSTEM.host_console_login()
+        self.cv_SYSTEM.host_console_unique_prompt()
+        l_res = c.run_command("lspci -mm -n")
+        self.pci_data_hardboot = '\n'.join(l_res) + '\n'
+        with open("pci_file_hardboot", 'w') as file:
+            for line in self.pci_data_hardboot:
+                file.write(line)
+        file.close()
+        # reboot from petitboot kernel
+        c.sol.sendline("reboot")
+        self.cv_SYSTEM.wait_for_petitboot()
+        self.cv_SYSTEM.set_state(OpSystemState.PETITBOOT)
+        self.cv_SYSTEM.goto_state(OpSystemState.PETITBOOT_SHELL)
+        self.cv_SYSTEM.host_console_unique_prompt()
+        l_res = c.run_command("lspci -mm -n")
+        self.pci_data_softreboot = '\n'.join(l_res) + '\n'
+        diff_process = subprocess.Popen(['diff', "-u", "pci_file_hardboot", "-"],
+                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        diff_stdout, diff_stderr = diff_process.communicate(self.pci_data_softreboot)
+        r = diff_process.wait()
+        self.assertEqual(r, 0, "Hard and Soft reboot PCI devices differ:\n%s%s" % (diff_stdout, diff_stderr))
+
+class TestPciOSReboot(TestPciSkirootReboot, unittest.TestCase):
+
+    def set_up(self):
+        self.test = "distro_reboot"
+
+class TestPciSkirootvsOS(TestPCI, unittest.TestCase):
+
+    def runTest(self):
+        c = self.cv_SYSTEM.sys_get_ipmi_console()
+        self.cv_SYSTEM.goto_state(OpSystemState.PETITBOOT_SHELL)
+        self.cv_SYSTEM.host_console_unique_prompt()
+        l_res = c.run_command("lspci -mm -n")
+        self.pci_data_skiroot = '\n'.join(l_res) + '\n'
+        with open("pci_file_skiroot", 'w') as file:
+            for line in self.pci_data_skiroot:
+                file.write(line)
+        file.close()
+        self.cv_SYSTEM.goto_state(OpSystemState.OFF)
+        self.cv_SYSTEM.goto_state(OpSystemState.OS)
+        self.cv_SYSTEM.host_console_login()
+        self.cv_SYSTEM.host_console_unique_prompt()
+        l_res = c.run_command("lspci -mm -n")
+        self.pci_data_hostos = '\n'.join(l_res) + '\n'
+        diff_process = subprocess.Popen(['diff', "-u", "pci_file_skiroot", "-"],
+                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        diff_stdout, diff_stderr = diff_process.communicate(self.pci_data_hostos)
+        r = diff_process.wait()
+        self.assertEqual(r, 0, "Skiroot and Host OS PCI devices differ:\n%s%s" % (diff_stdout, diff_stderr))
+
+def suite():
+    s = unittest.TestSuite()
+    s.addTest(TestPCIHost())
+    s.addTest(PcieLinkErrorsHost())
+    s.addTest(TestPCISkiroot())
+    s.addTest(PcieLinkErrorsSkiroot())
+    s.addTest(TestPciSkirootvsOS())
+    s.addTest(TestPciSkirootReboot())
+    s.addTest(TestPciOSReboot())
+    return s
 
