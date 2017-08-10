open-power/op-test

commit 8be6083b5bb99fe8023f956146fc4cf0f9d031bc

OpTestIPMI: fix close console path.
There may be situations State machine having the IPMIConsole
is in connected state but SOL console may drop in between.
So close console will try to close the non-existent console session.

So this patch fixes this issue by checking for the active
ipmi sol console session. this function closes the console
only when the session is active.

Signed-off-by: Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>

common/OpTestIPMI.py
@@ -112,6 +112,9 @@ class IPMIConsole():
     def close(self):
         if self.state == IPMIConsoleState.DISCONNECTED:
             return
+        if not self.sol.isalive():
+            print "IPMI SOL Console is already disconnected"
+            return
         try:
             self.sol.send("\r")
             self.sol.send('~.')

commit 57a671d109ed120f5f66042074546288eb18e837

OpTestIPMI: if SOL connection isn't alive, still do state machine ops
Fixes: 8be6083b5bb99fe8023f956146fc4cf0f9d031bc
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

common/OpTestIPMI.py
@@ -112,16 +112,17 @@ class IPMIConsole():
     def close(self):
         if self.state == IPMIConsoleState.DISCONNECTED:
             return
-        if not self.sol.isalive():
-            print "IPMI SOL Console is already disconnected"
-            return
         try:
+            if not self.sol.isalive():
+                print "IPMI SOL Console is already disconnected"
+                pass
             self.sol.send("\r")
             self.sol.send('~.')
             self.sol.expect('[terminated ipmitool]')
             self.sol.close()
         except pexpect.ExceptionPexpect:
             raise OpTestError("IPMI: failed to close ipmi console")
+
         self.sol.terminate()
         self.state = IPMIConsoleState.DISCONNECTED
 