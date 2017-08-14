open-power/op-test

commit 04ce5b751feb4c660c0dc61f4a3211d940022dcb

Improve wait_for_petitboot logic
(slightly) better throw some errors and attempt to make things
better when you don't reach petitboot.

Also, bump the timeout due to long IPL times on some systems with
verbose hostboot output.

Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

common/OpTestSystem.py
@@ -131,7 +131,7 @@ class OpTestSystem(object):
         self.state = state
 
     def goto_state(self, state):
-        print "OpTestSystem START STATE: %s" % (self.state)
+        print "OpTestSystem START STATE: %s (target %s)" % (self.state, state)
         while 1:
             self.state = self.stateHandlers[self.state](state)
             print "OpTestSystem TRANSITIONED TO: %s" % (self.state)
@@ -172,7 +172,12 @@ class OpTestSystem(object):
             self.sys_power_off()
             return OpSystemState.POWERING_OFF
 
-        self.wait_for_petitboot()
+        try:
+            self.wait_for_petitboot()
+        except pexpect.TIMEOUT:
+            self.sys_sel_check()
+            return OpSystemState.UNKNOWN
+
         # Once reached to petitboot check for any SEL events
         self.sys_sel_check()
         return OpSystemState.PETITBOOT
@@ -1223,10 +1228,25 @@ class OpTestSystem(object):
 
     def wait_for_petitboot(self):
         console = self.console.get_console()
-        # Wait for petitboot
-        console.expect('Petitboot', timeout=BMC_CONST.PETITBOOT_TIMEOUT)
-        console.expect('x=exit', timeout=10)
-        # there will be extra things in the pexpect buffer here
+        try:
+            # Wait for petitboot (for a *LOOONNNG* time due to verbose IPLs)
+            r = 1
+            t = 40
+            while r == 1 and t:
+                # TODO check for forward progress
+                r = console.expect(['Petitboot', pexpect.TIMEOUT], timeout=10)
+                if r == 1 and t == 0:
+                    raise pexpect.TIMEOUT
+
+            # Newer petitboot may render banner *after* the x=exit line
+            # So, work around that by asking for a redraw.
+            console.sendcontrol('l')
+            console.expect('x=exit', timeout=10)
+            # there will be extra things in the pexpect buffer here
+        except pexpect.TIMEOUT as e:
+            print "Timeout waiting for Petitboot!"
+            print str(e)
+            raise e
 
     def wait_for_kexec(self):
         console = self.console.get_console()

commit 453105b59b602ebbf6d431c96ccf4bc9ec834843

OpTestSystem: don't cancel autoboot on petitboot screen
Fixes: 04ce5b751feb4c660c0dc61f4a3211d940022dcb
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

common/OpTestSystem.py
@@ -1230,18 +1230,17 @@ class OpTestSystem(object):
         console = self.console.get_console()
         try:
             # Wait for petitboot (for a *LOOONNNG* time due to verbose IPLs)
+            seen = 0
             r = 1
             t = 40
-            while r == 1 and t:
+            while seen < 2 and t:
                 # TODO check for forward progress
-                r = console.expect(['Petitboot', pexpect.TIMEOUT], timeout=10)
-                if r == 1 and t == 0:
+                r = console.expect(['x=exit', 'Petitboot', pexpect.TIMEOUT], timeout=10)
+                if r == 2 and t == 0:
                     raise pexpect.TIMEOUT
+                if r in [0,1]:
+                    seen = seen + 1
 
-            # Newer petitboot may render banner *after* the x=exit line
-            # So, work around that by asking for a redraw.
-            console.sendcontrol('l')
-            console.expect('x=exit', timeout=10)
             # there will be extra things in the pexpect buffer here
         except pexpect.TIMEOUT as e:
             print "Timeout waiting for Petitboot!"