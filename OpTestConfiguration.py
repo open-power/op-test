open-power/op-test

commit bbdee26cb84339b6cbc19ac13cca1dd8b04443fb

Filter out control characters when logging to console
We will get a log file in test-results/ of all console activity
in the raw, but will now use 'cat -v' to escape control characters
that we print to stdout that we get from the host. This means that
the petitboot UI won't draw over your console when running 'op-test'.

We have a (in progress) '--quiet' option too.

Remove remnants of FFDC capture that didn't capture anything useful.

Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

OpTestConfiguration.py
@@ -15,6 +15,8 @@ from common.OpTestOpenBMC import HostManagement
 from common.OpTestWeb import OpTestWeb
 import argparse
 import time
+import subprocess
+import sys
 
 # Look at the addons dir for any additional OpTest supported types
 # If new type was called Kona, the layout would be as follows
@@ -56,6 +58,8 @@ class OpTestConfiguration():
                             help="Run a test suite(s)")
         tgroup.add_argument("--run", action='append',
                             help="Run individual tests")
+        tgroup.add_argument("--quiet", action='store_true', default=False,
+                            help="Don't splat lots of things to the console")
 
         parser.add_argument("--machine-state", help="Current machine state",
                             choices=['UNKNOWN', 'OFF', 'PETITBOOT',
@@ -96,9 +100,6 @@ class OpTestConfiguration():
         hostgroup.add_argument("--platform",
                                help="Platform (used for EnergyScale tests)",
                                choices=['unknown','habanero','firestone','garrison','firenze'])
-
-        ffdcgroup = parser.add_argument_group('FFDC', 'First Failure Data Capture')
-        ffdcgroup.add_argument("--ffdcdir", help="FFDC directory")
 
         imagegroup = parser.add_argument_group('Images', 'Firmware LIDs/images to flash')
         imagegroup.add_argument("--host-pnor", help="PNOR image to flash")
@@ -148,6 +149,27 @@ class OpTestConfiguration():
         else:
             self.outsuffix = time.strftime("%Y%m%d%H%M%S")
 
+        # set up where all the logs go
+        logfile = os.path.join(self.output,"%s.log" % self.outsuffix)
+        print "Log file: %s" % logfile
+        logcmd = "tee %s" % (logfile)
+        # we use 'cat -v' to convert control characters
+        # to something that won't affect the user's terminal
+        if self.args.quiet:
+            logcmd = logcmd + "> /dev/null"
+        else:
+            logcmd = logcmd + "| sed -u -e 's/\\r$//g'|cat -v"
+
+        print "logcmd: %s" % logcmd
+        self.logfile_proc = subprocess.Popen(logcmd,
+                                             stdin=subprocess.PIPE,
+                                             stderr=sys.stderr,
+                                             stdout=sys.stdout,
+                                             shell=True)
+        print repr(self.logfile_proc)
+        self.logfile = self.logfile_proc.stdin
+        self.logfile.write("Hello World\n")
+
         if self.args.machine_state == None:
             self.startState = OpSystemState.UNKNOWN
         else:
@@ -158,12 +180,14 @@ class OpTestConfiguration():
         host = OpTestHost(self.args.host_ip,
                           self.args.host_user,
                           self.args.host_password,
-                          self.args.bmc_ip)
+                          self.args.bmc_ip,
+                          logfile=self.logfile)
         if self.args.bmc_type in ['AMI', 'SMC']:
             ipmi = OpTestIPMI(self.args.bmc_ip,
                               self.args.bmc_usernameipmi,
                               self.args.bmc_passwordipmi,
-                              self.args.ffdcdir, host=host)
+                              host=host,
+                              logfile=self.logfile)
             web = OpTestWeb(self.args.bmc_ip,
                             self.args.bmc_usernameipmi,
                             self.args.bmc_passwordipmi)
@@ -172,6 +196,7 @@ class OpTestConfiguration():
                 bmc = OpTestBMC(ip=self.args.bmc_ip,
                                 username=self.args.bmc_username,
                                 password=self.args.bmc_password,
+                                logfile=self.logfile,
                                 ipmi=ipmi,
                                 web=web,
                 )
@@ -183,7 +208,6 @@ class OpTestConfiguration():
                                 web=web,
                 )
             self.op_system = OpTestSystem(
-                i_ffdcDir=self.args.ffdcdir,
                 state=self.startState,
                 bmc=bmc,
                 host=host,
@@ -192,14 +216,14 @@ class OpTestConfiguration():
             ipmi = OpTestIPMI(self.args.bmc_ip,
                               self.args.bmc_usernameipmi,
                               self.args.bmc_passwordipmi,
-                              self.args.ffdcdir, host=host)
+                              host=host,
+                              logfile=self.logfile)
             bmc = OpTestFSP(self.args.bmc_ip,
                             self.args.bmc_username,
                             self.args.bmc_password,
                             ipmi=ipmi,
             )
             self.op_system = OpTestFSPSystem(
-                i_ffdcDir=self.args.ffdcdir,
                 state=self.startState,
                 bmc=bmc,
                 host=host,
@@ -208,13 +232,15 @@ class OpTestConfiguration():
             ipmi = OpTestIPMI(self.args.bmc_ip,
                               self.args.bmc_usernameipmi,
                               self.args.bmc_passwordipmi,
-                              self.args.ffdcdir, host=host)
+                              host=host,
+                              logfile=self.logfile)
             rest_api = HostManagement(self.args.bmc_ip,
                                 self.args.bmc_username,
                                 self.args.bmc_password)
             bmc = OpTestOpenBMC(self.args.bmc_ip,
                                 self.args.bmc_username,
                                 self.args.bmc_password,
+                                logfile=self.logfile,
                                 ipmi=ipmi, rest_api=rest_api)
             self.op_system = OpTestOpenBMCSystem(
                 host=host,

common/OpTestASM.py
@@ -52,7 +52,7 @@ except AttributeError:
 
 class OpTestASM:
 
-    def __init__(self, i_fspIP, i_fspUser, i_fspPasswd, i_ffdcDir=None):
+    def __init__(self, i_fspIP, i_fspUser, i_fspPasswd):
         self.host_name = i_fspIP
         self.user_name = i_fspUser
         self.password = i_fspPasswd

common/OpTestBMC.py
@@ -50,14 +50,14 @@ class SSHConnectionState():
     CONNECTED = 1
 
 class OpTestBMC():
-    def __init__(self, ip=None, username=None, password=None, i_ffdcDir=None, ipmi=None, rest=None, web=None):
+    def __init__(self, ip=None, username=None, password=None, logfile=sys.stdout, ipmi=None, rest=None, web=None):
         self.cv_bmcIP = ip
         self.cv_bmcUser = username
         self.cv_bmcPasswd = password
-        self.cv_ffdcDir = i_ffdcDir
         self.cv_IPMI = ipmi
         self.rest = rest
         self.cv_WEB = web
+        self.logfile = logfile
         self.state = SSHConnectionState.DISCONNECTED
 
     def bmc_host(self):
@@ -80,7 +80,7 @@ class OpTestBMC():
         p.SSH_OPTS = p.SSH_OPTS + " -o 'StrictHostKeyChecking=no'"
         p.SSH_OPTS = p.SSH_OPTS + " -o 'UserKnownHostsFile /dev/null' "
         p.force_password = True
-        p.logfile = sys.stdout
+        p.logfile = self.logfile
         p.PROMPT = '# '
         self.pxssh = p
         return p
@@ -201,7 +201,7 @@ class OpTestBMC():
 
         print rsync_cmd
         rsync = pexpect.spawn(rsync_cmd)
-        rsync.logfile = sys.stdout
+        rsync.logfile = self.logfile
         rsync.expect('assword: ')
         rsync.sendline(self.cv_bmcPasswd)
         rsync.expect('total size is', timeout=1800)

common/OpTestFSP.py
@@ -53,9 +53,8 @@ class OpTestFSP():
     # @param i_fspIP @type string: IP Address of the FSP
     # @param i_fspUser @type string: Userid to log into the FSP
     # @param i_fspPasswd @type string: Password of the userid to log into the FSP
-    # @param i_ffdcDir @type string: Optional param to indicate where to write FFDC
     #
-    def __init__(self, i_fspIP, i_fspUser, i_fspPasswd, i_ffdcDir=None, ipmi=None, rest=None):
+    def __init__(self, i_fspIP, i_fspUser, i_fspPasswd, ipmi=None, rest=None):
         self.host_name = i_fspIP
         self.user_name = i_fspUser
         self.password = i_fspPasswd

common/OpTestHost.py
@@ -59,11 +59,12 @@ class SSHConnectionState():
     CONNECTED = 1
 
 class SSHConnection():
-    def __init__(self, ip=None, username=None, password=None):
+    def __init__(self, ip=None, username=None, password=None, logfile=sys.stdout):
         self.ip = ip
         self.username = username
         self.password = password
         self.state = SSHConnectionState.DISCONNECTED
+        self.logfile = logfile
 
     def new_pxssh(self):
         # pxssh has a nice 'echo=False' mode, but only
@@ -73,7 +74,7 @@ class SSHConnection():
         p.SSH_OPTS = p.SSH_OPTS + " -o 'StrictHostKeyChecking=no'"
         p.SSH_OPTS = p.SSH_OPTS + " -o 'UserKnownHostsFile /dev/null' "
         p.force_password = True
-        p.logfile = sys.stdout
+        p.logfile = self.logfile
         self.pxssh = p
         return p
 
@@ -152,18 +153,16 @@ class OpTestHost():
     # @param i_hostuser @type string: Userid to log into the host
     # @param i_hostpasswd @type string: Password of the userid to log into the host
     # @param i_bmcip @type string: IP Address of the bmc
-    # @param i_ffdcDir @type string:specifies the directory under which to save all FFDC data
     #
-    def __init__(self, i_hostip, i_hostuser, i_hostpasswd, i_bmcip, i_ffdcDir=None):
+    def __init__(self, i_hostip, i_hostuser, i_hostpasswd, i_bmcip, logfile=sys.stdout):
         self.ip = i_hostip
         self.user = i_hostuser
         self.passwd = i_hostpasswd
         self.util = OpTestUtil()
         self.bmcip = i_bmcip
-        self.cv_ffdcDir = i_ffdcDir
         parent_dir = os.path.dirname(os.path.abspath(__file__))
-        self.results_dir = self.cv_ffdcDir
-        self.ssh = SSHConnection(i_hostip, i_hostuser, i_hostpasswd)
+        self.logfile = logfile
+        self.ssh = SSHConnection(i_hostip, i_hostuser, i_hostpasswd, logfile=self.logfile)
 
     def hostname(self):
         return self.ip
@@ -270,7 +269,6 @@ class OpTestHost():
 
     ##
     # @brief It will gather OPAL Message logs and store the copy in a logfile
-    #        which will be stored in FFDC dir.
     #
     # @return BMC_CONST.FW_SUCCESS  or raise OpTestError
     #

common/OpTestIPMI.py
@@ -46,12 +46,13 @@ from Exceptions import BMCDisconnected
 
 class IPMITool():
     def __init__(self, method='lanplus', binary='ipmitool',
-                 ip=None, username=None, password=None):
+                 ip=None, username=None, password=None, logfile=sys.stdout):
         self.method = 'lanplus'
         self.ip = ip
         self.username = username
         self.password = password
         self.binary = binary
+        self.logfile = logfile
 
     def binary_name(self):
         return self.binary
@@ -98,10 +99,10 @@ class IPMIConsoleState():
     CONNECTED = 1
 
 class IPMIConsole():
-    def __init__(self, ipmitool=None, logdir=None, delaybeforesend=None):
+    def __init__(self, ipmitool=None, logfile=sys.stdout, delaybeforesend=None):
         self.ipmitool = ipmitool
         self.state = IPMIConsoleState.DISCONNECTED
-        self.logdir = logdir
+        self.logfile = logfile
         self.delaybeforesend = delaybeforesend
 
     def terminate(self):
@@ -139,7 +140,7 @@ class IPMIConsole():
 
         cmd = self.ipmitool.binary_name() + self.ipmitool.arguments() + ' sol activate'
         print cmd
-        solChild = pexpect.spawn(cmd,logfile=sys.stdout)
+        solChild = pexpect.spawn(cmd,logfile=self.logfile)
         self.state = IPMIConsoleState.CONNECTED
         self.sol = solChild
         if self.delaybeforesend:
@@ -275,17 +276,18 @@ class IPMIConsole():
         return output
 
 class OpTestIPMI():
-    def __init__(self, i_bmcIP, i_bmcUser, i_bmcPwd, i_ffdcDir, host=None, delaybeforesend=None):
+    def __init__(self, i_bmcIP, i_bmcUser, i_bmcPwd, logfile=sys.stdout, host=None, delaybeforesend=None):
         self.cv_bmcIP = i_bmcIP
         self.cv_bmcUser = i_bmcUser
         self.cv_bmcPwd = i_bmcPwd
-        self.cv_ffdcDir = i_ffdcDir
+        self.logfile = logfile
         self.ipmitool = IPMITool(method='lanplus',
                                  ip=i_bmcIP,
                                  username=i_bmcUser,
-                                 password=i_bmcPwd)
+                                 password=i_bmcPwd,
+                                 logfile=logfile)
         self.console = IPMIConsole(ipmitool=self.ipmitool,
-                                   logdir=i_ffdcDir,
+                                   logfile=logfile,
                                    delaybeforesend=delaybeforesend)
         self.util = OpTestUtil()
         self.host = host
@@ -601,11 +603,9 @@ class OpTestIPMI():
     def ipmi_sel_check(self, i_string="Transition to Non-recoverable"):
         output = self.ipmitool.run('sel elist')
 
-        if self.cv_ffdcDir:
-            logFile = self.cv_ffdcDir + '/' + 'host_sel_elist.log'
-            with open('%s' % logFile, 'w') as f:
-                for line in output:
-                    f.write(line)
+        if self.logfile:
+            for line in output:
+                self.logfile.write(line)
 
         if i_string in output:
             raise OpTestError('Error log(s) detected during IPL: %s' % output)

common/OpTestOpenBMC.py
@@ -49,12 +49,13 @@ class ConsoleState():
     CONNECTED = 1
 
 class HostConsole():
-    def __init__(self, host, username, password, port=22):
+    def __init__(self, host, username, password, logfile=sys.stdout, port=22):
         self.state = ConsoleState.DISCONNECTED
         self.host = host
         self.username = username
         self.password = password
         self.port = port
+        self.logfile = logfile
 
     def terminate(self):
         if self.state == ConsoleState.CONNECTED:
@@ -90,7 +91,7 @@ class HostConsole():
                + " -l %s %s" % (self.username, self.host)
            )
         print cmd
-        solChild = pexpect.spawn(cmd,logfile=sys.stdout)
+        solChild = pexpect.spawn(cmd,logfile=self.logfile)
         self.state = ConsoleState.CONNECTED
         self.sol = solChild
         return solChild
@@ -719,17 +720,18 @@ class HostManagement():
 
 
 class OpTestOpenBMC():
-    def __init__(self, ip=None, username=None, password=None, ipmi=None, rest_api=None):
+    def __init__(self, ip=None, username=None, password=None, ipmi=None, rest_api=None, logfile=sys.stdout):
         self.hostname = ip
         self.username = username
         self.password = password
         self.ipmi = ipmi
         self.rest_api = rest_api
         self.has_vpnor = None
+        self.logfile = logfile
         # We kind of hack our way into pxssh by setting original_prompt
         # to also be \n, which appears to fool it enough to allow us
         # continue.
-        self.console = HostConsole(ip, username, password, port=2200)
+        self.console = HostConsole(ip, username, password, port=2200, logfile=self.logfile)
         self.bmc = OpTestBMC(ip=self.hostname,
                             username=self.username,
                             password=self.password)

common/OpTestSystem.py
@@ -62,14 +62,13 @@ class OpTestSystem(object):
     #  @param i_bmcPasswd The password of the userid to log into the BMC with
     #  @param i_bmcUserIpmi The userid to issue the BMC IPMI commands with
     #  @param i_bmcPasswdIpmi The password of BMC IPMI userid
-    #  @param i_ffdcDir Optional param to indicate where to write FFDC
     #
     # "Only required for inband tests" else Default = None
     # @param i_hostIP The IP address of the Host
     # @param i_hostuser The userid to log into the Host
     # @param i_hostPasswd The password of the userid to log into the host with
     #
-    def __init__(self,i_ffdcDir=None,
+    def __init__(self,
                  bmc=None, host=None,
                  state=OpSystemState.UNKNOWN):
         self.bmc = self.cv_BMC = bmc
@@ -1279,13 +1278,11 @@ class OpTestSystem(object):
 
 class OpTestFSPSystem(OpTestSystem):
     def __init__(self,
-                 i_ffdcDir=None,
                  host=None,
                  bmc=None,
                  state=OpSystemState.UNKNOWN):
         bmc.fsp_get_console()
-        super(OpTestFSPSystem, self).__init__(i_ffdcDir=i_ffdcDir,
-                                              host=host,
+        super(OpTestFSPSystem, self).__init__(host=host,
                                               bmc=bmc,
                                               state=state)
 
@@ -1315,17 +1312,15 @@ class OpTestFSPSystem(OpTestSystem):
 
 class OpTestOpenBMCSystem(OpTestSystem):
     def __init__(self,
-                 i_ffdcDir=None,
                  host=None,
                  bmc=None,
                  state=OpSystemState.UNKNOWN):
         # Ensure we grab host console early, in order to not miss
         # any messages
         self.console = bmc.get_host_console()
-        super(OpTestOpenBMCSystem, self).__init__(i_ffdcDir=i_ffdcDir,
-                                              host=host,
-                                              bmc=bmc,
-                                              state=state)
+        super(OpTestOpenBMCSystem, self).__init__(host=host,
+                                                  bmc=bmc,
+                                                  state=state)
     # REST Based management
     def sys_inventory(self):
         self.rest.get_inventory()
@@ -1385,17 +1380,15 @@ class OpTestOpenBMCSystem(OpTestSystem):
 
 class OpTestQemuSystem(OpTestSystem):
     def __init__(self,
-                 i_ffdcDir=None,
                  host=None,
                  bmc=None,
                  state=OpSystemState.UNKNOWN):
         # Ensure we grab host console early, in order to not miss
         # any messages
         self.console = bmc.get_host_console()
-        super(OpTestQemuSystem, self).__init__(i_ffdcDir=i_ffdcDir,
-                                              host=host,
-                                              bmc=bmc,
-                                              state=state)
+        super(OpTestQemuSystem, self).__init__(host=host,
+                                               bmc=bmc,
+                                               state=state)
 
     def sys_wait_for_standby_state(self, i_timeout=120):
         self.bmc.power_off()

commit 66cf3245018730e75308c0f0952caaef5fbe3467

Don't echo 'hello World' to console on start
Fixes: bbdee26cb84339b6cbc19ac13cca1dd8b04443fb
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

OpTestConfiguration.py
@@ -188,7 +188,6 @@ class OpTestConfiguration():
                                              shell=True)
         print repr(self.logfile_proc)
         self.logfile = self.logfile_proc.stdin
-        self.logfile.write("Hello World\n")
 
         if self.args.machine_state == None:
             self.startState = OpSystemState.UNKNOWN