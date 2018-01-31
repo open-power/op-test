12ba82b1d0f5d32edb68964e139ca14c085c1490
Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>
2018-01-31 16:06:13 +0530

    Sbe passthrough testcase.
    Whenever SBE has passthrough command it interrupts the host to process
    the command. So this test does injecting interrupt to doorbell register
    using putscom which OPAL has to receive and process it, and calls the
    HBRT functionality to process the passthrough command.
    
    Signed-off-by: Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>

--- common/OpTestSystem.py
+++ common/OpTestSystem.py
@@ -1369,6 +1369,9 @@ class OpTestOpenBMCSystem(OpTestSystem):
         # Deleting complete SEL repository is not yet implemented
         #self.rest.clear_sel()
 
+    def sys_get_sel_list(self):
+        self.rest.list_sel()
+
     def sys_sel_check(self):
         self.rest.list_sel()
 
