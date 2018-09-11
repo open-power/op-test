8970495d2bf2e67fd21a5ef3b6e6b086f606da5e
Stewart Smith <stewart@linux.ibm.com>
2018-09-11 17:51:34 +1000

    Exceptions: Make PEP8 happy
    Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

--- common/Exceptions.py
+++ common/Exceptions.py
@@ -25,6 +25,7 @@ These exceptions are used throughout op-test to help testing error
 conditions as well as failing tests when unexpected errors occur.
 '''
 
+
 class CommandFailed(Exception):
     '''
     Running a command (BMC or Host) failed with non-zero exit code.
@@ -35,7 +36,9 @@ class CommandFailed(Exception):
         self.exitcode = exitcode
 
     def __str__(self):
-        return "Command '%s' exited with %d.\nOutput:\n%s" % (self.command, self.exitcode, self.output)
+        return "Command '%s' exited with %d.\nOutput:\n%s".format(
+            self.command, self.exitcode, self.output)
+
 
 class SSHSessionDisconnected(Exception):
     '''
@@ -43,8 +46,10 @@ class SSHSessionDisconnected(Exception):
     '''
     def __init__(self, notice):
         self.notice = notice
+
     def __str__(self):
         return "SSH session/console disconnected due to '%s'" % self.notice
+
 
 class LoginFailure(Exception):
     '''
@@ -52,8 +57,10 @@ class LoginFailure(Exception):
     '''
     def __init__(self, notice):
         self.notice = notice
+
     def __str__(self):
         return "Login failure due to '%s'" % self.notice
+
 
 class BMCDisconnected(Exception):
     '''
@@ -61,8 +68,10 @@ class BMCDisconnected(Exception):
     '''
     def __init__(self, notice):
         self.notice = notice
+
     def __str__(self):
         return "BMC console disconnected due to '%s'" % self.notice
+
 
 class NoKernelConfig(Exception):
     '''
@@ -72,8 +81,11 @@ class NoKernelConfig(Exception):
     def __init__(self, kernel, path):
         self.kernel = kernel
         self.path = path
+
     def __str__(self):
-        return "kernel config for %s not found, looked for %s" % (self.kernel, self.path)
+        return "kernel config for %s not found, looked for %s".format(
+            self.kernel, self.path)
+
 
 class KernelModuleNotLoaded(Exception):
     '''
@@ -81,8 +93,10 @@ class KernelModuleNotLoaded(Exception):
     '''
     def __init__(self, module):
         self.module = module
+
     def __str__(self):
         return "Kernel module %s not loaded" % (self.module)
+
 
 class KernelConfigNotSet(Exception):
     '''
@@ -91,20 +105,25 @@ class KernelConfigNotSet(Exception):
     '''
     def __init__(self, opt):
         self.opt = opt
+
     def __str__(self):
         return "Kernel config %s not present" % (self.opt)
 
+
 class KernelSoftLockup(Exception):
     '''
-    We caught a soft lockup. Mostly this will mean we need to fail the test and reboot.
-    Some test cases may *intentionally* cause these, so they can catch them and act
-    appropriately.
+    We caught a soft lockup. Mostly this will mean we need to fail the test
+    and reboot.
+    Some test cases may *intentionally* cause these, so they can catch them
+    and act appropriately.
     '''
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Soft lockup (machine in state %s): %s" % (self.state, self.log)
+
 
 class KernelHardLockup(Exception):
     '''
@@ -113,47 +132,62 @@ class KernelHardLockup(Exception):
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Hard lockup (machine in state %s): %s" % (self.state, self.log)
 
+
 class KernelOOPS(Exception):
     '''
-    We detected a kernel OOPS. Mostly this will mean we need to fail the test and reboot.
-    Some test cases may *intentionally* cause these, so they can catch them and act
-    appropriately.
+    We detected a kernel OOPS. Mostly this will mean we need to fail the test
+    and reboot.
+    Some test cases may *intentionally* cause these, so they can catch them
+    and act appropriately.
     '''
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Kernel OOPS (machine in state %s): %s" % (self.state, self.log)
 
+
 class KernelKdump(Exception):
     '''
-    We observe a Kdump kernel booting after a kernel crash, to dump vmcore for debug.
+    We observe a Kdump kernel booting after a kernel crash, to dump vmcore
+    for debug.
     '''
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
-        return "Kernel Kdump (machine in state %s): %s" % (self.state, self.log)
+        return "Kernel Kdump (machine in state %s): %s".format(
+            self.state, self.log)
+
 
 class KernelCrashUnknown(Exception):
     '''
-    Kernel crashed but it didn't reach the end failure condition(i.e a timeout occured)
+    Kernel crashed but it didn't reach the end failure condition
+    (i.e a timeout occured)
     '''
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
-        return "Kernel crash unknown state (machine in state %s): %s" % (self.state, self.log)
+        return "Kernel crash unknown state (machine in state %s): %s".format(
+            self.state, self.log)
+
 
 class KernelBug(Exception):
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Kernel bug in state %s: %s" % (self.state, self.log)
+
 
 class SkibootAssert(Exception):
     '''
@@ -162,8 +196,10 @@ class SkibootAssert(Exception):
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Hit skiboot assert in state %s: %s" % (self.state, self.log)
+
 
 class SkibootException(Exception):
     '''
@@ -172,8 +208,11 @@ class SkibootException(Exception):
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
-        return "Hit skiboot unexpected exception in state %s: %s" % (self.state, self.log)
+        return "Hit skiboot unexpected exception in state %s: %s".format(
+            self.state, self.log)
+
 
 class KernelPanic(Exception):
     '''
@@ -182,18 +221,23 @@ class KernelPanic(Exception):
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Kernel panic in state %s: %s" % (self.state, self.log)
 
+
 class PlatformError(Exception):
     '''
-    We detected a system reboot due to platform error(i.e checkstop, machine check, MCE, etc)
+    We detected a system reboot due to platform error (i.e checkstop,
+    machine check, MCE, etc)
     '''
     def __init__(self, state, log):
         self.log = log
         self.state = state
+
     def __str__(self):
         return "Platform error at state %s. Log: %s" % (self.state, self.log)
+
 
 class HostbootShutdown(Exception):
     '''
@@ -201,6 +245,7 @@ class HostbootShutdown(Exception):
     '''
     def __str__(self):
         return "Detected hostboot got IPMI shutdown request"
+
 
 class UnexpectedCase(Exception):
     '''
@@ -210,14 +255,17 @@ class UnexpectedCase(Exception):
         default_vals = {'state': None, 'msg': None}
         self.kwargs = {}
         for key in default_vals:
-          if key not in kwargs.keys():
-            self.kwargs[key] = default_vals[key]
-          else:
-            self.kwargs[key] = kwargs[key]
+            if key not in kwargs.keys():
+                self.kwargs[key] = default_vals[key]
+            else:
+                self.kwargs[key] = kwargs[key]
 
     def __str__(self):
         return ('Something unexpected happened in State=\"{}\"'
-                ' Review the following for more details\nMessage=\"{}\"'.format(self.kwargs['state'], self.kwargs['msg']))
+                ' Review the following for more details\n'
+                'Message=\"{}\"'.format(
+                    self.kwargs['state'], self.kwargs['msg']))
+
 
 class WaitForIt(Exception):
     '''
@@ -227,14 +275,17 @@ class WaitForIt(Exception):
         default_vals = {'expect_dict': None, 'reconnect_count': 0}
         self.kwargs = {}
         for key in default_vals:
-          if key not in kwargs.keys():
-            self.kwargs[key] = default_vals[key]
-          else:
-            self.kwargs[key] = kwargs[key]
+            if key not in kwargs.keys():
+                self.kwargs[key] = default_vals[key]
+            else:
+                self.kwargs[key] = kwargs[key]
 
     def __str__(self):
-      return ('Waiting for "{}" did not succeed, check the loop_max if needing to wait longer'
-           ' (number of reconnect attempts were {})'.format(self.kwargs['expect_dict'], self.kwargs['reconnect_count']))
+        return ('Waiting for "{}" did not succeed, check the loop_max if '
+                'needing to wait longer (number of reconnect attempts '
+                'were {})'.format(self.kwargs['expect_dict'],
+                                  self.kwargs['reconnect_count']))
+
 
 class RecoverFailed(Exception):
     '''
@@ -244,15 +295,19 @@ class RecoverFailed(Exception):
         default_vals = {'before': None, 'after': None, 'msg': None}
         self.kwargs = {}
         for key in default_vals:
-          if key not in kwargs.keys():
-            self.kwargs[key] = default_vals[key]
-          else:
-            self.kwargs[key] = kwargs[key]
+            if key not in kwargs.keys():
+                self.kwargs[key] = default_vals[key]
+            else:
+                self.kwargs[key] = kwargs[key]
 
     def __str__(self):
-      return ('Unable to get the proper prompt, probably just retry'
-           ' review the following for more details\nExpect Before Buffer=\"{}\"\nExpect After Buffer=\"{}\"'
-           '\nMessage=\"{}\"'.format(self.kwargs['before'], self.kwargs['after'], self.kwargs['msg']))
+        return ('Unable to get the proper prompt, probably just retry'
+                ' review the following for more details\n'
+                'Expect Before Buffer=\"{}\"\nExpect After Buffer=\"{}\"'
+                '\nMessage=\"{}\"'.format(self.kwargs['before'],
+                                          self.kwargs['after'],
+                                          self.kwargs['msg']))
+
 
 class UnknownStateTransition(Exception):
     '''
@@ -262,21 +317,29 @@ class UnknownStateTransition(Exception):
         default_vals = {'state': None, 'msg': None}
         self.kwargs = {}
         for key in default_vals:
-          if key not in kwargs.keys():
-            self.kwargs[key] = default_vals[key]
-          else:
-            self.kwargs[key] = kwargs[key]
+            if key not in kwargs.keys():
+                self.kwargs[key] = default_vals[key]
+            else:
+                self.kwargs[key] = kwargs[key]
 
     def __str__(self):
-        return ('Something happened system state=\"{}\" and we transitioned to UNKNOWN state. '
-                ' Review the following for more details\nMessage=\"{}\"'.format(self.kwargs['state'], self.kwargs['msg']))
+        return ('Something happened system state=\"{}\" and we '
+                'transitioned to UNKNOWN state. '
+                ' Review the following for more details\n'
+                'Message=\"{}\"'.format(self.kwargs['state'],
+                                        self.kwargs['msg']))
+
 
 class StoppingSystem(Exception):
     '''
-    We have either set the system to stop for some condition or reached the kill_cord limit and stopping.
+    We have either set the system to stop for some condition or reached the
+    kill_cord limit and stopping.
     '''
     def __str__(self):
-        return "System has either set the stop flag for some condition or reached the kill_cord limit on trying to continue, check that your system is operational"
+        return "System has either set the stop flag for some condition or "\
+            "reached the kill_cord limit on trying to continue, check that "\
+            "your system is operational"
+
 
 class ConsoleSettings(Exception):
     '''
@@ -286,12 +349,16 @@ class ConsoleSettings(Exception):
         default_vals = {'before': None, 'after': None, 'msg': None}
         self.kwargs = {}
         for key in default_vals:
-          if key not in kwargs.keys():
-            self.kwargs[key] = default_vals[key]
-          else:
-            self.kwargs[key] = kwargs[key]
+            if key not in kwargs.keys():
+                self.kwargs[key] = default_vals[key]
+            else:
+                self.kwargs[key] = kwargs[key]
 
     def __str__(self):
-      return ("Setting the prompt or logging in for the console was not successful, check credentials and review the following"
-              " for more details\nExpect Before Buffer=\"{}\"\nExpect After Buffer=\"{}\" \nMessage=\"{}\""
-              "".format(self.kwargs['before'], self.kwargs['after'], self.kwargs['msg']))
+        return ("Setting the prompt or logging in for the console was not "
+                "successful, check credentials and review the following"
+                " for more details\nExpect Before Buffer=\"{}\"\n"
+                "Expect After Buffer=\"{}\" \nMessage=\"{}\""
+                "".format(self.kwargs['before'],
+                          self.kwargs['after'],
+                          self.kwargs['msg']))
