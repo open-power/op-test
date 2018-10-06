957c8c556a012e5295b4bc471d51f398b6cb9d4c
Deb McLemore <debmc@linux.ibm.com>
2018-10-06 07:19:40 -0500

    DeviceTreeValidation: Diff and compare enhancements
    Improve the comparisons made between skiroot and host DT.
    
    Signed-off-by: Deb McLemore <debmc@linux.ibm.com>

--- testcases/DeviceTreeValidation.py
+++ testcases/DeviceTreeValidation.py
@@ -236,24 +236,124 @@ class DeviceTreeValidation(unittest.TestCase):
                             "Non monotonocity observed for pstate ids")
 
     def check_dt_matches(self):
+        # allows the ability to filter for debug
+        # skip the parent(s) in the hierarchy
+        # leave as None to get checked
+        # setting to anything other than None will ignore
+        ignore_dict = {"/proc/device-tree/ibm,opal/"              : None,
+                       "/proc/device-tree/ibm,opal/sensor-groups" : None,
+                       "/proc/device-tree/ibm,opal/sensors"       : None,
+                       "/proc/device-tree/ibm,opal/power-mgt"     : None,
+                      }
         if len(prop_val_pair_skiroot) and len(prop_val_pair_host):
-            failures = []
+            unified_failed_props = missing_prop_in_skiroot = \
+                 missing_prop_in_host = matched_prop_in_host = 0
+            host_props = unified_diff_passed = \
+                 skipped_props = skiroot_props = 0
+            unified_failures = []
+            missing_in_host = []
+            missing_in_skiroot = []
+            matched_in_host = []
+            skiroot_check = all(elem in prop_val_pair_host \
+                for elem in prop_val_pair_skiroot)
+            host_check = all(elem in prop_val_pair_skiroot \
+                for elem in prop_val_pair_host)
+            for prop in prop_val_pair_host:
+                host_props += 1
+                if prop not in prop_val_pair_skiroot:
+                    # these failures will show up in the host_check
+                    # log the specifics
+                    missing_prop_in_skiroot += 1
+                    missing_in_skiroot.append(["DT Node \"{}\" found in host"
+                       " but does NOT exist in skiroot".format(prop)])
+                    log.debug("DT Node \"{}\" found in host"
+                       " but does NOT exist in skiroot".format(prop))
             for prop in prop_val_pair_skiroot:
+                skiroot_props += 1
                 if prop in prop_val_pair_host:
-                    if prop_val_pair_skiroot[prop] in prop_val_pair_host[prop]:
-                        log.debug("Node {} is the same in both host and "
-                                  "skiroot".format(prop))
-                    else:
-                        failures.append(
-                            difflib.unified_diff(prop_val_pair_skiroot[prop],
-                                                 prop_val_pair_host[prop],
-                                                 fromfile="skiroot",
-                                                 tofile="host"))
+                    check1_len = len(prop_val_pair_skiroot[prop])
+                    check2_len = len(prop_val_pair_host[prop])
+                    check1_skiroot = all(elem in prop_val_pair_host[prop] \
+                        for elem in prop_val_pair_skiroot[prop])
+                    check2_skiroot = all(elem in prop_val_pair_skiroot[prop] \
+                        for elem in prop_val_pair_host[prop])
+                    # if cross check good and same length (just in case
+                    # duplicate list items) trying to catch any mods)
+                    # if order of list content differs, the unified diff
+                    # always flags as difference even though
+                    # the elements match (just in a different order)
+                    if check1_skiroot and check2_skiroot \
+                      and (check1_len == check2_len):
+                        matched_prop_in_host += 1
+                        matched_in_host.append(["Skiroot DT Node \"{}\" found"
+                           " in skiroot and exists in host".format(prop)])
+                        log.debug("DT Node \"{}\" is the same in both"
+                                  " skiroot and host".format(prop))
+                        continue
+                    if ignore_dict.get(str(prop)) != None:
+                        skipped_props += 1
+                        continue
+                    unified_output = difflib.unified_diff(
+                        filter(None, prop_val_pair_skiroot[prop]),
+                        filter(None, prop_val_pair_host[prop]),
+                        fromfile="skiroot",
+                        tofile="host",
+                        lineterm="")
+                    unified_list = list(unified_output)
+                    if len(unified_list):
+                        unified_failed_props += 1
+                        unified_failures.append(["<----------> Diff Failure #{}: \"{}\""
+                            .format(unified_failed_props, prop)])
+                        unified_failures.append(unified_list)
+                    else: # diff is OK, should not get here
+                        # good cases should have been filtered above
+                        unified_diff_passed += 1
+                        log.debug("Unexpected case unified_diff passed={}"
+                            .format(prop))
                 else:
-                    failures.append("Node %s is not existed in host OS" % prop)
-            if failures:
-                self.assertTrue(False, "DT Property values pair vaildation "
-                                "failed: {}".format(repr(failures)))
+                    # these failures will show up in the skiroot_check
+                    # log the specifics
+                    missing_prop_in_host += 1
+                    missing_in_host.append(["DT Node \"{}\" does not"
+                       " exist in the host OS".format(prop)])
+            if unified_failed_props \
+              or (skiroot_props != host_props) \
+              or not skiroot_check \
+              or not host_check:
+                for i in unified_failures:
+                    line_num = 1
+                    for j in i:
+                        log.debug("Failure Line #{} {}".format(line_num,
+                            "".join(j)))
+                        line_num += 1
+                self.assertTrue(False, "DT Diff failures detected Total={}"
+                                "\nSUMMARY:\n{}\n"
+                                "Missing (found in skiroot not in host):\n{}\n"
+                                "Missing (found in host not in skiroot):\n{}\n"
+                                "Skipped Nodes (usually 0 or 1, the parent) = {}\n"
+                                "Cross Check matched (skiroot to host) = {}\n"
+                                "Skiroot Nodes = {}\n"
+                                "Host Nodes = {}\n"
+                                "Skiroot Nodes matched host Nodes = {}\n"
+                                "Host Nodes matched skiroot Nodes = {}\n"
+                                "Failures:{}\n"
+                                .format(
+                                 unified_failed_props,
+                                ('\n'.join(i for f in unified_failures for i in f)),
+                                (None if len(missing_in_host) == 0 \
+                                     else ('\n'.join(i for f in missing_in_host for i in f))),
+                                (None if len(missing_in_skiroot) == 0 \
+                                     else ('\n'.join(i for f in missing_in_skiroot for i in f))),
+                                 skipped_props,
+                                 matched_prop_in_host,
+                                 skiroot_props,
+                                 host_props,
+                                 skiroot_check,
+                                 host_check,
+                                 unified_failed_props,
+                                ))
+            else:
+                log.debug("DT Diff success")
 
     def runTest(self):
         self.skipTest("Not meant to be run directly. "
@@ -281,8 +381,7 @@ class DeviceTreeValidationSkiroot(DeviceTreeValidation):
         # We can extend for other nodes as well, which are suspicieous.
         props = self.c.run_command("find %s -type d" % self.node)
         for prop in props:
-            prop_val_pair_skiroot[prop] = "".join(
-                self.c.run_command("lsprop %s" % prop)[2:])
+            prop_val_pair_skiroot[prop] = self.c.run_command("lsprop %s" % prop)
 
         self.check_dt_matches()
 
@@ -302,7 +401,6 @@ class DeviceTreeValidationHost(DeviceTreeValidation):
 
         props = self.c.run_command("find %s -type d" % self.node)
         for prop in props:
-            prop_val_pair_host[prop] = "".join(
-                self.c.run_command("lsprop %s" % prop)[1:])
+            prop_val_pair_host[prop] = self.c.run_command("lsprop %s" % prop)
 
         self.check_dt_matches()
