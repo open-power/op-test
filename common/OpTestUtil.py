afeba1de3d314d8ef15ee8f575f6c5e795d775f2
rashijhawar <rashi@linux.vnet.ibm.com>
2022-02-08 13:24:11 +0530

    Added a wait_for() function
    Added a wait_for() function in OpTestUtil.py
    
    Signed-off-by: rashijhawar <rashi@linux.vnet.ibm.com>

--- common/OpTestUtil.py
+++ common/OpTestUtil.py
@@ -1835,6 +1835,36 @@ class OpTestUtil():
         stdout_value = self.cronus_subcommand(command=command, minutes=minutes)
         return stdout_value
 
+    def wait_for(self, func, timeout, first=0.0, step=1.0, text=None, args=None, kwargs=None):
+        """
+        Wait until func() evaluates to True.
+
+        If func() evaluates to True before timeout expires, return the
+        value of func(). Otherwise return None.
+
+        :param timeout: Timeout in seconds
+        :param first: Time to sleep before first attempt
+        :param step: Time to sleep between attempts in seconds
+        :param text: Text to print while waiting, for debug purposes
+        :param args: Positional arguments to func
+        :param kwargs: Keyword arguments to func
+        """
+        if args is None:
+            args = []
+        if kwargs is None:
+            kwargs = {}
+        start_time = time.monotonic()
+        end_time = start_time + timeout
+        time.sleep(first)
+        while time.monotonic() < end_time:
+            if text:
+                log.debug("%s (%.9f secs)", text, (time.monotonic() - start_time))
+            output = func(*args, **kwargs)
+            if output:
+                return output
+            time.sleep(step)
+        return None
+
 
 class Server(object):
     '''
