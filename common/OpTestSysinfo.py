836e7f9c40f46b48187f2c2f2adef9293db7249c
yeswanth <yeswanth@linux.ibm.com>
2025-06-03 11:34:17 +0530

    Sysinfo utility to record system information
    This utility is designed to collect information across different
    test stack like OS, HMC, VIOS sysinfo etc..
    sysinfo collected for every run before the test starts.
    
    The flow is as below:
    The OpTestSysinfo has different functions for different stack
    like OS, HMC, VIOS. Whenever the PS1 is initiated, the OpTestSysinfo
    module collects the data from the respective stack.
    
    Signed-off-by: yeswanth <yeswanth@linux.ibm.com>

---
+++ common/OpTestSysinfo.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: op-test-framework/common/OpTestUtil.py $
+#
+# OpenPOWER Automated Test Project
+#
+# Contributors Listed Below - COPYRIGHT 2015
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+import sys
+import os
+import datetime
+import time
+import pwd
+import string
+import subprocess
+import random
+import re
+import telnetlib
+import socket
+import select
+import time
+import pty
+import pexpect
+import subprocess
+import requests
+import traceback
+from requests.adapters import HTTPAdapter
+from http.client import HTTPConnection
+import urllib3  # setUpChildLogger enables integrated logging with op-test
+import json
+import tempfile
+
+import yaml
+
+from common.OpTestSSH import OpTestSSH
+
+import logging
+import OpTestLogger
+log = OpTestLogger.optest_logger_glob.get_logger(__name__)
+
+
+config_file = "/root/op-test/CONFIG.YAML"
+
+
+class OpTestSysinfo():
+
+    def __init__(self):
+        with open(config_file, "r") as file:
+            self.config_actions = yaml.safe_load(file)
+            list_of_commands = self.config_actions["LINUX"]["COMMANDS"]
+            list_of_hmc_commands = self.config_actions["LINUX"]["HMC-COMMANDS"]
+            #print(list_of_commands)
+    def get_OSconfig(self, pty, prompt):
+        # Collect config related data from the OS
+        try:
+            list_of_commands = self.config_actions["LINUX"]["COMMANDS"]
+            print("########### OS Sysinfo ########")
+            for index, each_cmd in enumerate(list_of_commands, start=0):
+                pty.sendline(each_cmd)
+                rc = pty.expect([prompt, pexpect.TIMEOUT, pexpect.EOF], timeout=10)
+        except CommandFailed as cf:
+            raise cf
+    def get_HMCconfig(self, pty, prompt):
+        # Collect config data from HMC
+        ################ HMC INFO ####################
+        #Details of HMC software version, release, service pack, and build level
+        list_of_hmc_commands = self.config_actions["LINUX"]["HMC-COMMANDS"]
+        print("Getting HMC software version, release, service pack, and build level")
+        allsysinfo=[]
+        for index, each_cmd in enumerate(list_of_hmc_commands, start=0):
+             if re.search('SYS', each_cmd):
+                 for ele in allsysinfo:
+                     new_cmd=re.sub('SYS',ele,each_cmd)
+                     try:
+                         output = pty.run_command(new_cmd)
+                     except Exception as e: 
+                          print('command failed due to system error')
+             else:
+                output = pty.run_command(each_cmd)
+                if re.search('list', str(type(output)) ):
+                    allsysinfo = list(output)
+                    print('ALL SYSINFO NAMES',allsysinfo)
+
+
