open-power/op-test

commit e617ed1c73d48687e83908894b197e8e5213a8dc

gitbisect: get online cpu count instead of cpu cores
Add method to get online cpus instead of cpu cores
replace host_get_core_count with host_get_online_cpus

Signed-off-by: Abdul Haleem <abdhalee@linux.vnet.ibm.com>

common/OpTestHost.py
@@ -830,6 +830,15 @@ class OpTestHost():
         res = self.host_run_command("lscpu --all -e| wc -l", console=console)
         return int(res[0])/(self.host_get_smt(console=console))
 
+   def host_get_online_cpus(self, console=0):
+       '''
+       Get total online cpu count
+       return : cpus
+       type: int
+       '''
+       res = self.host_run_command("lscpu --online -e | wc -l", console=console)
+       return int(res[0])-1
+
     def host_gather_debug_logs(self, console=0):
         self.host_run_command(
             "grep ',[0-4]\]' /sys/firmware/opal/msglog", console=console)

testcases/OpTestKernelTest.py
@@ -106,7 +106,7 @@ class KernelTest(unittest.TestCase):
         log.info("Upstream kernel commit-time: %s", tcommit)
         log.debug("Compile the upstream kernel")
         try:
-            cpu = self.cv_HOST.host_get_core_count()
+            cpu = self.cv_HOST.host_get_online_cpus()
             err=self.con.run_command("make -j {} -s".format(int(cpu)), timeout=self.host_cmd_timeout)
             log.info("Kernel build successful")
             return 0,err
@@ -130,7 +130,7 @@ class KernelTest(unittest.TestCase):
         self.con.run_command("make olddefconfig", timeout=60)
         base_version = self.con.run_command("uname -r")
         ker_ver = self.con.run_command("make kernelrelease")[-1]
-        cpu = self.cv_HOST.host_get_core_count()
+        cpu = self.cv_HOST.host_get_online_cpus()
         self.con.run_command("make -j {} -s".format(int(cpu)), timeout=60000)
         self.con.run_command("make modules_install", timeout=300)
         self.con.run_command("make install", timeout=120)

commit 1701100fbbc8360414e5bbe3049ede70145644e7

Fix IndentationError caused by commit e617ed1
The Commit e617ed1 causes Indentation Error:
IndentationError: unindent does not match any outer indentation level

This Patch fixes the above error.

Signed-off-by: Misbah Anjum N <misanjumn@ibm.com>

common/OpTestHost.py
@@ -830,14 +830,14 @@ class OpTestHost():
         res = self.host_run_command("lscpu --all -e| wc -l", console=console)
         return int(res[0])/(self.host_get_smt(console=console))
 
-   def host_get_online_cpus(self, console=0):
-       '''
-       Get total online cpu count
-       return : cpus
-       type: int
-       '''
-       res = self.host_run_command("lscpu --online -e | wc -l", console=console)
-       return int(res[0])-1
+    def host_get_online_cpus(self, console=0):
+        '''
+        Get total online cpu count
+        return : cpus
+        type: int
+        '''
+        res = self.host_run_command("lscpu --online -e | wc -l", console=console)
+        return int(res[0])-1
 
     def host_gather_debug_logs(self, console=0):
         self.host_run_command(